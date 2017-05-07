6681d94767c0b6fce310ac1aa9556bd3bb53596f
Cameron <CameronBradley3D@gmail.com>
2017-05-07 16:37:47 +1000

    deleted old bat files

--- Run_FramesToVideo.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-
-cd /d %~dp0
-"%~dp0Python34\python.exe" "%~dp0AutoUpload.py" %* 
-PAUSE
--- Run_FramesToYoutube.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-cd /d %~dp0
-"%~dp0Python34\python.exe" "%~dp0AutoUpload.py" %* -upload
-PAUSE
